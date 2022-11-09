9a6b680c07752fe1ebc4e7005f2e7e37d41b7134
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2022-11-09 09:08:22 +0100

    Add files via upload

---
+++ KlaseObjektiPon/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlaseObjektiPon
+{
+    class Stablo
+    {
+        bool otpadajuListovi;
+        string vrstaStabla;
+
+        //set
+        public void setListovi(bool otpdajuListovi)
+        {
+            this.otpadajuListovi = otpadajuListovi;
+        }
+
+        //get
+        public bool getListovi()
+        {
+            return this.otpadajuListovi;
+        }
+
+        public override string ToString()
+        {
+            return "Stablo je " + this.vrstaStabla;
+
+        }
+
+        public Stablo()
+        { }
+        //konstruktor
+        public Stablo(bool otpadajuListovi)
+        {
+            this.otpadajuListovi = otpadajuListovi;
+            if (otpadajuListovi)
+            {
+                this.vrstaStabla = "bijelogorično";
+
+            }
+            else
+            {
+                this.vrstaStabla = "crnogorično";
+
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stablo stablo1 = new Stablo(true);
+            Stablo stablo2 = new Stablo(false);
+
+            Console.WriteLine(stablo1.ToString());
+            Console.WriteLine(stablo2.ToString());
+            Console.ReadKey();
+        }
+
+
+    }
+}
